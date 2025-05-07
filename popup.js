f232cbde106f8b3e721ca57a5512cdfd2bff9bdb
Jdrao7 <hellojdrao@gmail>
2025-05-07 23:04:00 +0530

    Added some basic interface for popup

---
+++ popup.js
@@ -0,0 +1,7 @@
+document.getElementById("summarizer-button").addEventListener("click",()=>{
+    console.log("Summarizer Clicked!")
+})
+
+document.getElementById("copy-button").addEventListener("click",()=>{
+    console.log("Copy Clicked!")
+})
