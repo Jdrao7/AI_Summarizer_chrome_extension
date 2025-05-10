06e1d91d26e373d917fc984807a3af5b32911c67
Jdrao7 <hellojdrao@gmail>
2025-05-10 13:06:58 +0530

    edited popup.js

--- popup.js
+++ popup.js
@@ -1,7 +1,14 @@
 document.getElementById("summarizer-button").addEventListener("click",()=>{
-    console.log("Summarizer Clicked!")
-})
 
-document.getElementById("copy-button").addEventListener("click",()=>{
-    console.log("Copy Clicked!")
-})
+    const result=document.getElementById("result");
+    result.textContent="Extracting text...";
+
+    chrome.tabs.query({active:true,currentWindow:true},([tab])=>{
+        chrome.tabs.sendMessage(tab.id,{type:"GET_ARTICLE_TEXT"},({text})=>{
+            result.textContent=text
+            ?text.slice(0,300)+"...":"No article text found.";
+             }
+        );
+    
+    });
+});
